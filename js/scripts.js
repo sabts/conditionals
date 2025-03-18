f46737d99601bc14fff0d12caf236e96dc9188b8
sabts <sabrinataraschi@gmail.com>
2025-03-18 11:59:08 +0100

    Update scripts.js

---
+++ js/scripts.js
@@ -0,0 +1,7 @@
+const minimumHeightRollarcoaster = (minimumHeight) =>{
+    let minimumHeight = 150
+    if ( minimumHeigh > 150 )
+        console.log (minimumHeight)
+    }
+
+minimumHeightRollarcoaster(150)
