6178ef6dcb359b94893a7f40bc9fab12e623793f
sabts <sabrinataraschi@gmail.com>
2025-03-18 14:57:53 +0100

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,26 @@
 const minimumHeightRollarcoaster = (minimumHeight) =>{
-    let minimumHeight = 150
-    if ( minimumHeigh > 150 )
-        console.log (minimumHeight)
-    }
+    if (minimumHeight >= 150) {
+        return 'Camila se puede montar en la atracción'} else { return 'Camila no puede subir a la atracción'}; }
+        
+console.log (minimumHeightRollarcoaster(170))
+console.log (minimumHeightRollarcoaster(147))
 
-minimumHeightRollarcoaster(150)
+const begoFreeShipping = (freeShipping) =>{
+    if (freeShipping <= 50) {return 'Bego tendrá que pagar el shipping'} else { return 'El envio de Bego es gratis'};
+}
+console.log (begoFreeShipping(55))
+console.log (begoFreeShipping(45))
+
+const multipleOfSeven = (number) =>{
+    if (number % 7 === 0){return 'El número es multiplo de 7 '} else {return 'No es multiplo de 7'
+    }
+}
+console.log (multipleOfSeven(40))
+console.log (multipleOfSeven(70))
+
+const abbyrecruitmentTest = (number) =>{
+    if (number > 8) {return 'Salimos al amanecer'} if (number >=5){return 'Entras en la reserva'} if(number <=4){return 'Estás Fuera'}
+}
+console.log (abbyrecruitmentTest (3))
+console.log (abbyrecruitmentTest (6))
+console.log (abbyrecruitmentTest (9))
